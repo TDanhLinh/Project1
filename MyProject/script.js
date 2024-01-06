9265b2f47dd03bb61284f4cbce739e2844fc008e
TDanhLinh <143780100+TDanhLinh@users.noreply.github.com>
2024-01-07 01:01:10 +0700

    Done

--- MyProject/script.js
+++ MyProject/script.js
@@ -5,7 +5,6 @@ function minDistance(dist, V, sptSet) {
 
     for (let v = 0; v < V; v++) {
         if (sptSet[v] == false && dist[v] <= min) {
-            //console.log('Test: ' + v + ' ' + dist[v]);
             min = dist[v];
             min_index = v;
         }
@@ -13,11 +12,15 @@ function minDistance(dist, V, sptSet) {
     return min_index;
 }
 
+// Hàm hiển thị đồ thị sau khi tìm được đường đi ngắn nhất bằng Dijkstra và lưu lại output vào file .txt
 function printSolutionDijkstra(trace, V, graph, src, dest) {
+    // Khởi tạo vùng lưu giá trị output vào 1 thẻ textarea có id = outputArea trong index.html để lưu vào file .txt
     const textArea = document.getElementById("outputArea");
+    // Khởi tạo data cho node và edge phục vụ cho việc hiển thị đồ thị lên màn hình
     let nodes = new vis.DataSet();
     let edges = new vis.DataSet();
 
+    // Kiểm tra đường đi đã được đi qua hay chưa
     let check = [];
     for (let i = 0; i < V; i++) {
         check[i] = [];
@@ -26,48 +29,44 @@ function printSolutionDijkstra(trace, V, graph, src, dest) {
         }
     }
 
-    for (let i = 0; i < V; i++) {
+    // Thiết lập các node để hiển thị lên màn hình
+    for (let i = 1; i <= V; i++) {
         nodes.add({ id: i, label: '' + i });
     }
 
-    //for (let i = 0; i < V; i++) {
-
-    // Trace back to construct the path
+    // Truy vết đường đi
     let path = [];
     let vertex = dest;
     let sum = 0;
     if (trace[vertex] === undefined) {
-        alert('Không tìm thấy được đi ngắn nhất từ ' + src + ' đến ' + dest);
-        textArea.value = 'Không tìm thấy được đi ngắn nhất từ ' + src + ' đến ' + dest;
-        //console.log('1: ' + textArea.value);
+        // Gán giá trị cho textArea để lưu vào file txt
+        // Coi textArea.value như string để lưu giá trị vào file txt
+        textArea.value = 'Không tìm thấy được đi ngắn nhất từ ' + (src + 1) + ' đến ' + (dest + 1);
     }
     else {
-        textArea.value = 'Đường đi ngắn nhất từ đỉnh ' + src + ' đến đỉnh ' + dest + ' là: \n';
-        //console.log('2: ' +textArea.value);
+        textArea.value = 'Đường đi ngắn nhất từ đỉnh ' + (src + 1) + ' đến đỉnh ' + (dest + 1) + ' là: \n';
         while (vertex !== undefined) {
-            path.push(vertex);
+            path.push(vertex + 1);
             if (trace[vertex] !== undefined) {
                 if (check[trace[vertex]][vertex] === 0) {
-                    //console.log('vertex = ' + vertex + ' trace = ' + trace[vertex] + ' ' + graph[trace[vertex]][vertex]);
-                    edges.add({ from: trace[vertex], to: vertex, label: graph[trace[vertex]][vertex].toString() });
+                    // Thiết lập các cạnh để hiển thị lên màn hình
+                    edges.add({ from: trace[vertex] + 1, to: vertex + 1, label: graph[trace[vertex]][vertex].toString() });
                     check[trace[vertex]][vertex] = 1;
                     sum += graph[trace[vertex]][vertex];
                 }
             }
             vertex = trace[vertex];
         }
-        //console.log(path.reverse().join(" -> "));
+        // Gán giá trị cho textArea để lưu vào file txt
+        // Coi textArea.value như string để lưu giá trị vào file txt
         textArea.value += path.reverse().join(' -> ') + ' ';
-        //console.log('3: ' + textArea.value);
         textArea.value += '\nĐộ dài đường đi ngắn nhất là: ' + sum;
-        //console.log('4: ' + textArea.value);
     }
 
     for (let i = 0; i < V; i++) {
         for (let j = 0; j < V; j++) {
             if (check[i][j] === 0 && graph[i][j] !== 0) {
-                //console.log('i = ' + i + ' j = ' + j + ' ' + graph[i][j]);
-                edges.add({ from: i, to: j, label: graph[i][j].toString(), color: 'black' });
+                edges.add({ from: i + 1, to: j + 1, label: graph[i][j].toString(), color: 'black' });
             }
         }
     }
@@ -105,7 +104,6 @@ function printSolutionDijkstra(trace, V, graph, src, dest) {
 }
 
 function dijkstra(graph, V, src, dest) {
-    //console.log("dijkstra work! " + V + ' ' + src + ' ' + dest);
     let dist = new Array(V);
     let sptSet = new Array(V);
     let trace = new Array(V);
@@ -116,18 +114,15 @@ function dijkstra(graph, V, src, dest) {
     }
 
     dist[src] = 0;
-    //console.log('src = ' + src);
 
     for (let count = 0; count < V - 1; count++) {
         let u = minDistance(dist, V, sptSet);
-        //console.log('u = ' + u);
         sptSet[u] = true;
 
         for (let v = 0; v < V; v++) {
             if (!sptSet[v] && graph[u][v] != 0 && dist[u] != Number.MAX_VALUE && dist[u] + graph[u][v] < dist[v]) {
                 dist[v] = dist[u] + graph[u][v];
                 trace[v] = u;
-                //console.log('trace: ' + trace[v]);
             }
         }
     }
@@ -135,8 +130,11 @@ function dijkstra(graph, V, src, dest) {
     printSolutionDijkstra(trace, V, graph, src, dest);
 }
 
-function printSolutionBellmanFord(graph, dis, V, E, src) {
+// Lưu output sau khi chạy thuật toán BellmanFord và xuất ra file txt
+function printSolutionBellmanFord(graph, dis, V, E, src, trace) {
+    // Khởi tạo vùng lưu giá trị output vào 1 thẻ textarea có id = outputArea trong index.html để lưu vào file .txt
     const textArea = document.getElementById("outputArea");
+    /*
     for (let i = 0; i < E; i++) {
         for (let u = 0; u < V; u++) {
             for (let v = 0; v < V; v++) {
@@ -147,17 +145,41 @@ function printSolutionBellmanFord(graph, dis, V, E, src) {
             }
         }
     }
+    */
 
-    textArea.value = 'Khoảng cách ngắn nhất từ đỉnh: ' + src + ' đến các đỉnh khác là: \n';
+    // Gán giá trị cho textArea để lưu vào file txt
+    // Coi textArea.value như string để lưu giá trị vào file txt
+    textArea.value = 'Khoảng cách ngắn nhất từ đỉnh ' + (src + 1) + ' đến các đỉnh khác là: \n';
     for (let i = 0; i < V; i++) {
         if (dis[i] !== 0) {
-            textArea.value += 'Đỉnh ' + i + ':   ' + dis[i] + '\n';
+            // Truy vết đường đi
+            let path = [];
+            let vertex = i;
+            if (trace[vertex] === undefined) {
+                // Gán giá trị cho textArea để lưu vào file txt
+                // Coi textArea.value như string để lưu giá trị vào file txt
+                textArea.value += 'Không tìm thấy được đi ngắn nhất từ ' + (src + 1) + ' đến ' + (i + 1);
+            }
+            else {
+                textArea.value += 'Đỉnh ' + (i + 1) + ':   ' + dis[i] + '\t';
+                textArea.value += 'Đường đi ngắn nhất từ đỉnh ' + (src + 1) + ' đến đỉnh ' + (i + 1) + ' là: ';
+                while (vertex !== undefined) {
+                    path.push(vertex + 1);
+                    vertex = trace[vertex];
+                }
+                // Gán giá trị cho textArea để lưu vào file txt
+                // Coi textArea.value như string để lưu giá trị vào file txt
+                textArea.value += path.reverse().join(' -> ') + ' ';
+            }
+            textArea.value += '\n';
         }
     }
 }
 
+// Thuật toán Bellman Ford
 function BellmanFord(graph, V, E, src) {
     let dis = Array(V).fill(Number.MAX_VALUE);
+    let trace = new Array(V);
 
     dis[src] = 0;
 
@@ -165,29 +187,70 @@ function BellmanFord(graph, V, E, src) {
         for (let j = 0; j < E; j++) {
             for (let u = 0; u < V; u++) {
                 for (let v = 0; v < V; v++) {
-                    console.log('u = ' + u + 'v = ' + v + ' graph: ' + graph[u][v]);
                     if (graph[u][v] !== 0) {
-                        if ((dis[u] + graph[u][v]) < dis[v])
+                        if ((dis[u] + graph[u][v]) < dis[v]) {
                             dis[v] = dis[u] + graph[u][v];
+                            trace[v] = u;
+                        }
                     }
                 }
             }
         }
     }
-    printSolutionBellmanFord(graph, dis, V, E, src);
+    printSolutionBellmanFord(graph, dis, V, E, src, trace);
 }
 
-function printSolutionFloyd(dist, V) {
+// Lưu ouput sau khi chạy thuật toán Floyd và xuất ra file txt
+function printSolutionFloyd(dist, V, trace) {
+    // Khởi tạo vùng lưu giá trị output vào 1 thẻ textarea có id = outputArea trong index.html để lưu vào file .txt
     const textArea = document.getElementById("outputArea");
+    // Gán giá trị cho textArea để lưu vào file txt
+    // Coi textArea.value như string để lưu giá trị vào file txt
     textArea.value = 'Ma trận khoảng cách ngắn nhất giữa mọi cặp đỉnh là: \n';
     for (let i = 0; i < V; i++) {
         for (let j = 0; j < V; j++) {
-            console.log('i = ' + i + 'j = ' + j + 'dist = ' + dist[i][j]);
-            if (dist[i][j] === Number.MAX_VALUE) {
-                textArea.value += '\t∞\t';
+            if (j === 0) {
+                if (dist[i][j] === Number.MAX_VALUE) {
+                    textArea.value += '∞\t';
+                }
+                else {
+                    textArea.value += dist[i][j] + '\t';
+                }
             }
             else {
-                textArea.value += '\t' + dist[i][j] + '\t';
+                if (dist[i][j] === Number.MAX_VALUE) {
+                    textArea.value += '\t∞\t';
+                }
+                else {
+                    textArea.value += '\t' + dist[i][j] + '\t';
+                }
+            }
+        }
+        textArea.value += '\n';
+    }
+    textArea.value += '\n';
+    
+    // Truy vết lại để tạo ra ma trận P
+    // Gán giá trị cho textArea để lưu vào file txt
+    // Coi textArea.value như string để lưu giá trị vào file txt
+    textArea.value += 'Ma trận P là: \n';
+    for (let i = 0; i < V; i++) {
+        for (let j = 0; j < V; j++) {
+            if (j === 0) {
+                if (dist[i][j] === Number.MAX_VALUE) {
+                    textArea.value += '∞\t';
+                }
+                else {
+                    textArea.value += (trace[i][j] + 1) + '\t';
+                }
+            }
+            else {
+                if (dist[i][j] === Number.MAX_VALUE) {
+                    textArea.value += '\t∞\t';
+                }
+                else {
+                    textArea.value += '\t' + (trace[i][j] + 1) + '\t';
+                }
             }
         }
         textArea.value += '\n';
@@ -195,17 +258,18 @@ function printSolutionFloyd(dist, V) {
 }
 
 function floyd(graph, V) {
-    //console.log("Floyd work!");
     let dist = Array.from(Array(V), () => new Array(V).fill(0));
+    let trace = Array.from(Array(V), () => new Array(V));
+
     for (let i = 0; i < V; i++) {
         for (let j = 0; j < V; j++) {
+            trace[i][j] = i;
             if (graph[i][j] === 0) {
                 dist[i][j] = Number.MAX_VALUE;
             }
             else {
                 dist[i][j] = graph[i][j];
             }
-            //console.log('i = ' + i + ' j = ' + j + ' dist = ' + dist[i][j]);
         }
     }
 
@@ -218,19 +282,20 @@ function floyd(graph, V) {
                 else {
                     if (dist[i][j] >= (dist[i][k] + dist[k][j]) && (dist[k][j] != Number.MAX_VALUE && dist[i][k] != Number.MAX_VALUE)) {
                         dist[i][j] = dist[i][k] + dist[k][j];
+                        trace[i][j] = trace[k][j];
                     }
                 }
             }
         }
     }
 
-    printSolutionFloyd(dist, V);
+    printSolutionFloyd(dist, V, trace);
 }
 
 // Lưu đồ thị bằng biến toàn cục
 let Graph = [];
-let V;
-let E;
+let V; // Lưu số đỉnh bằng biến toàn cục
+let E; // Lưu số cạnh bằng biến toàn cục
 
 // Sinh ra đồ thị mới có số đỉnh, số cạnh và trọng số chạy từ Min -> Max theo người dùng nhập
 function newGraph() {
@@ -252,31 +317,11 @@ function newGraph() {
             graph[i][j] = 0;
         }
     }
-    /*
-    console.log(vertex);
-    console.log(edge);
-    console.log(weightMin);
-    console.log(weightMax);
- 
-    let nodes = new vis.DataSet([
-        { id: 1, label: "Node 1" },
-        { id: 2, label: "Node 2" },
-        { id: 3, label: "Node 3" },
-        { id: 4, label: "Node 4" },
-        { id: 5, label: "Node 5" },
-    ]);
-    let edges = new vis.DataSet([
-        { from: 1, to: 2, label: "21" },
-        { from: 1, to: 3, label: "13" },
-        { from: 2, to: 4, label: "24" },
-        { from: 2, to: 5 },
-        { from: 3, to: 4 },
-    ]);
-    */
+
     let nodes = new vis.DataSet();
     let edges = new vis.DataSet();
     // Khởi tạo và hiển thị các đỉnh
-    for (let i = 0; i < vertex; i++) {
+    for (let i = 1; i <= vertex; i++) {
         nodes.add({ id: i, label: '' + i });
     }
 
@@ -308,8 +353,7 @@ function newGraph() {
 
         textArea.value += (u - 1) + ' ' + (v - 1) + ' ' + w + '\n'; // Ghi các dòng tiếp theo
 
-        edges.add({ from: (u - 1), to: (v - 1), label: w.toString() }); // Thêm cạnh u,v với trọng số w vào data
-        //console.log('u = ' + (u - 1) + '; v = ' + (v - 1) + '; w = ' + w + '; Min = ' + weightMin + '; Max = ' + weightMax);
+        edges.add({ from: u, to: v, label: w.toString() }); // Thêm cạnh u,v với trọng số w vào data
     }
 
     for (let i = 0; i < vertex; i++) {
@@ -352,6 +396,51 @@ function newGraph() {
     let network = new vis.Network(container, data, options);
 }
 
+// Phần xử lý output 
+const button = document.querySelector("#startButton");
+const inputStartElement = document.getElementById("Start");
+const inputEndElement = document.getElementById("End");
+
+button.addEventListener("click", () => {
+    const inputStartValue = Number(inputStartElement.value);
+    const inputEndValue = Number(inputEndElement.value);
+
+    // Chọn ra thuật toán
+    const selectElement = document.querySelector(".form-select");
+    //selectElement.addEventListener("change", () => {
+
+    const selectedValue = selectElement.selectedOptions[0].value;
+
+    // Gọi thuật toán tương ứng dựa trên lựa chọn
+    if (selectedValue === "1") {
+        dijkstra(Graph, V, inputStartValue - 1, inputEndValue - 1);
+    } else if (selectedValue === "2") {
+        BellmanFord(Graph, V, E, inputStartValue - 1);
+    } else if (selectedValue === "3") {
+        floyd(Graph, V);
+    }
+    // Phần này tương tự downloadInputFile
+    const link = document.createElement("a");
+    const content = document.getElementById("outputArea").value;
+    const file = new Blob([content], { type: 'text/plain' });
+    link.href = URL.createObjectURL(file);
+    link.download = "output.txt";
+    link.click();
+    URL.revokeObjectURL(link.href);
+});
+
+// Thêm event cho Lưu File Button khi bấm vào sẽ sinh data cho input file rồi tự tải xuống
+const downloadInputFile = () => {
+    const link = document.createElement("a"); // Tạo ra 1 thẻ a
+    const content = document.getElementById("inputArea").value; // Set content cho thẻ a đó là 1 thẻ textArea có id = inputArea
+    const file = new Blob([content], { type: 'text/plain' }); // Tạo ra 1 file với định dạng là text và có content được khởi tạo ở trên
+    link.href = URL.createObjectURL(file); // Gắn url vào thẻ a
+    link.download = "input.txt"; // Thêm phương thức download cho link và set tên có file download = input.txt
+    link.click(); // Thêm event click để thực hiện thao tác download vào cho link
+    URL.revokeObjectURL(link.href); // Gỡ phương thức download ra khỏi link để reset
+};
+
+// Phần này là để ngăn button hoạt động khi chưa nhập đủ input và chưa có đồ thị
 const vertexInput = document.getElementById("Vertex");
 const edgeInput = document.getElementById("Edge");
 const weightMinInput = document.getElementById("MinWeight");
@@ -471,52 +560,6 @@ saveButton.style.display = "none";
 startButton.disabled = true;
 saveButton.disabled = true;
 
-const button = document.querySelector("#startButton");
-const inputStartElement = document.getElementById("Start");
-const inputEndElement = document.getElementById("End");
-
-button.addEventListener("click", () => {
-    const inputStartValue = Number(inputStartElement.value);
-    const inputEndValue = Number(inputEndElement.value);
-    console.log(inputStartValue + ' ' + inputEndValue);
-
-    // Chọn ra thuật toán
-    const selectElement = document.querySelector(".form-select");
-    //selectElement.addEventListener("change", () => {
-
-    const selectedValue = selectElement.selectedOptions[0].value;
-    //console.log("Selected value:", selectedValue);
-
-    // Call appropriate function based on the selected value:
-    if (selectedValue === "1") {
-        dijkstra(Graph, V, inputStartValue, inputEndValue);
-    } else if (selectedValue === "2") {
-        BellmanFord(Graph, V, E, inputStartValue);
-    } else if (selectedValue === "3") {
-        floyd(Graph, V);
-    }
-    const link = document.createElement("a");
-    const content = document.getElementById("outputArea").value;
-    console.log("test: " + content);
-    const file = new Blob([content], { type: 'text/plain' });
-    link.href = URL.createObjectURL(file);
-    link.download = "output.txt";
-    link.click();
-    URL.revokeObjectURL(link.href);
-    //});
-});
-
-// Thêm event cho Lưu File Button khi bấm vào sẽ sinh data cho input file rồi tự tải xuống
-const downloadInputFile = () => {
-    const link = document.createElement("a");
-    const content = document.getElementById("inputArea").value;
-    const file = new Blob([content], { type: 'text/plain' });
-    link.href = URL.createObjectURL(file);
-    link.download = "input.txt";
-    link.click();
-    URL.revokeObjectURL(link.href);
-};
-
 // Khởi tạo để kèm dòng note dưới button bằng bootstrap
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
@@ -524,5 +567,6 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 const inputArea = document.getElementById("inputArea");
 const outputArea = document.getElementById("outputArea");
 
+// Ẩn các thẻ textarea
 inputArea.style.display = "none";
 outputArea.style.display = "none";
